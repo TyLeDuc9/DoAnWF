da7950171abace9b8336533d5d4482c5219ccedb
Le Duc Ty <ducty9963@gmail.com>
2024-10-23 21:13:41 +0700

    project

---
+++ TyNhanLongKhoi_GUI/frmBaoCaoThongKe.cs
@@ -0,0 +1,96 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TyNhanLongKhoi_BUS;
+using TyNhanLongKhoi_DAL;
+
+namespace TyNhanLongKhoi_GUI
+{
+    public partial class frmBaoCaoThongKe : DevExpress.XtraEditors.XtraForm
+    {
+        public frmBaoCaoThongKe()
+        {
+            InitializeComponent();
+            thongKeLibrary = new ThongKeLibrary();
+
+
+        }
+        private ThongKeLibrary thongKeLibrary;
+
+
+
+
+        private async void frmBaoCaoThongKe_Load(object sender, EventArgs e)
+        {
+            await LoadAllBooks();
+        }
+
+        private async void radSachChoMuon_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (radSachChoMuon.Checked)
+            {
+                await LoadBorrowedBooks();
+            }
+        }
+
+        private async void radTatCaSach_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (radTatCaSach.Checked)
+            {
+                await LoadAllBooks();
+            }
+
+        }
+
+        private async Task LoadAllBooks()
+        {
+            var books = await thongKeLibrary.GetAllBooksAsync();
+
+            // Chọn các thuộc tính cụ thể để hiển thị trong DataGridView
+            var displayBooks = books.Select(b => new
+            {
+                b.MaSach,
+                b.TenSach,
+                b.MaTacGia,
+                b.MaNXB,
+                b.TheLoai,
+                b.SoLuong
+            }).ToList();
+
+            dataGridView1.DataSource = displayBooks;
+        }
+
+
+        private async Task LoadBorrowedBooks()
+        {
+            var borrowedBooks = await thongKeLibrary.GetBorrowedBooksAsync();
+            var displayChiTiet = borrowedBooks.Select(c => new
+            {
+                c.MaPhieu,
+                c.SoThe,
+                c.MaSach,
+                c.TinhTrang,
+                c.NgayMuon,
+                c.NgayTra,
+                c.SLSachMuon
+            }).ToList();
+            dataGridView1.DataSource=displayChiTiet;
+        }
+
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TyNhanLongKhoi_GUI/frmDangNhap.cs
@@ -0,0 +1,75 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TyNhanLongKhoi_BUS;
+
+namespace TyNhanLongKhoi_GUI
+{
+    public partial class frmDangNhap : DevExpress.XtraEditors.XtraForm
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string username = txtDangNhap.Text;
+            string password = txtMatKhau.Text;
+
+            DangNhapLibrary loginService = new DangNhapLibrary();
+            if (loginService.ValidateLogin(username, password))
+            {
+                this.Hide();
+                frmTrangChu f = new frmTrangChu();
+                f.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát", "Confirm", MessageBoxButtons.YesNo
+             , MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void txtDangNhap_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void txtMatKhau_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void chkMatKhau_CheckedChanged(object sender, EventArgs e)
+        {
+            txtMatKhau.UseSystemPasswordChar = !chkMatKhau.Checked;
+        }
+
+        private void frmDangNhap_Load(object sender, EventArgs e)
+        {
+            txtDangNhap.Text = string.Empty;
+            txtMatKhau.Text = string.Empty;
+            chkMatKhau.Checked = false;
+        }
+    }
+}
